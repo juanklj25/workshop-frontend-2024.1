1d8054bdb2a460a35a187542a1f1e6a61e7abb99
juanklj25 <delaruansousa@gmail.com>
2024-03-09 16:19:31 -0300

    feito 1 e 2 completo

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,6 @@ export default function Home() {
     backgroundColor: 'black',
   }
   const [shows, setShows] = useState([]);
-  const [Imagens, setImagens] = useState([]);
   
   useEffect (() => {
     fetch('https://api.tvmaze.com/shows')
@@ -16,17 +15,8 @@ export default function Home() {
     .then((data) => setShows(data))
     .catch((error) => console.log('error', error))
   }, [])
-
-
-  useEffect (()=>{
-    fetch('https://api.tvmaze.com/shows/1/images')
-    .then((responsa) => responsa.json())
-    .then((data)=> setImagens(data))
-    .catch((error)=> console.log('erro',error))
-  },[])
   
   console.log(shows);
-  console.log(Imagens);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black"> 
@@ -35,14 +25,9 @@ export default function Home() {
           <div key={show.id}>
             <h2 className="flex flex-col gap-y-5 text-center">{show.name}</h2>
             <h2 className="py-5 text-white font-bolt text-x1 rounded-xl text-center">{show.summary}</h2>
+            <img src={show.url} alt="show imagen"></img>
           </div>
         ))}
-        {Imagens.map((image)=>(
-          <div key={image.id} className="flex flex-col gap-y-5">
-            <img src={image.url} alt="show imagen"></img>
-        </div>
-        )
-        )}
       </div>
     </main>
   );
